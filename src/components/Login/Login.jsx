c24728e6e1c4d2d29974520aabee42554774f85c
kiarash jamali <kiajamali978@gmail.com>
2023-03-06 20:47:17 -0800

    Add react-toastify to this project

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import "./../../../node_modules/animate.css/animate.min.css";
 import validate from "../validate";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
 
@@ -9,12 +11,50 @@ const Login = () => {
     const [errors, setErrors] = useState({});
     const [touched, setTouched] = useState({});
 
+    // Change handler
     const changeHandler = ({ target }) => {
         setData({ ...data, [target.name]: target.value });
     }
 
+    // Touch handler
     const touchedHandler = ({ target }) => {
         setTouched({ ...touched, [target.name]: true });
+    }
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        if (Object.keys(errors).length) {
+            toast("Invalid data", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                type: "error",
+                theme: "colored",
+            });
+
+            setTouched({
+                name: true,
+                password: true
+            });
+
+        } else {
+            toast("Successfully", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                type: "success",
+                theme: "colored",
+            });
+        }
     }
 
     useEffect(() => {
@@ -27,7 +67,8 @@ const Login = () => {
                 {/*Title*/}
                 <h1 className={styles.title}>Login To Site</h1>
 
-                <form action="" method="get" className={styles.form}>
+                <form action="#" method="get" className={styles.form}
+                    onSubmit={submitHandler}>
                     {/*User name*/}
                     <div>
                         <label htmlFor="name">Name</label>
@@ -52,11 +93,13 @@ const Login = () => {
 
                     {/*Buttons*/}
                     <div className={styles.buttons}>
-                        <a href="">Login</a>
+                        <button type="submit">Login</button>
                         <a href="">Sign up</a>
                     </div>
                 </form>
+
             </div>
+            <ToastContainer />
         </>
     )
 }
